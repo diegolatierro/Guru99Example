1b546bbf7565c5e2b3f8b8c0d6734345a379cace
diegolatierro <diegolatierro@gmail.com>
2020-06-16 17:45:10 -0300

    baseTest changes

--- src/base/BaseTest.java
+++ src/base/BaseTest.java
@@ -14,14 +14,17 @@ public class BaseTest {
 	
     String driverPath = "C:\\geckodriver.exe";
     String chromedriverPath = "C:\\chromedriver.exe";
+    String chromedriverPathLinux = "/usr/bin/chromedriver";
 	 protected WebDriver driver;
 	 @BeforeMethod
 	    protected void setup() {
-	    	//System.setProperty("webdriver.gecko.driver", driverPath);
-	    	System.setProperty("webdriver.chrome.driver", chromedriverPath);
+	    	//System.setProperty("webdriver.gecko.driver", driverPath); //check if necessary on AWS - set geckodriver en usr/bin
+		 	// en setProperty agregar headless
+	    	System.setProperty("webdriver.chrome.driver", chromedriverPathLinux);
 	        //remote webdiver search
 	        //driver = new FirefoxDriver();
 	    	driver = new ChromeDriver();
+	    	
 	        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	    }
 	    @AfterMethod
