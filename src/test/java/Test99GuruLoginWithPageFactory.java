dbbc12e1aa13bdc73406ce7c95913bd2b3f0b706
diegolatierro <diegolatierro@gmail.com>
2020-06-17 13:23:42 -0300

    baseTest changes

--- src/test/java/Test99GuruLoginWithPageFactory.java
+++ src/test/java/Test99GuruLoginWithPageFactory.java
@@ -42,7 +42,7 @@ public class Test99GuruLoginWithPageFactory extends BaseTest{
 
     //login to application
 
-    objLogin.loginToGuru99("mgr123", "mgr!23");
+    objLogin.loginToGuru99("mgr123", "mgr!234");
 
     // go the next page
 
